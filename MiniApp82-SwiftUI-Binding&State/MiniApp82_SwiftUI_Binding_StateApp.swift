267a3edc3b8d4d33e7a7717df7e813dc09aea348
Kota <maedakoutaa@gmail.com>
2022-08-14 11:21:17 +0900

    Initial Commit

---
+++ MiniApp82-SwiftUI-Binding&State/MiniApp82_SwiftUI_Binding_StateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp82_SwiftUI_Binding_StateApp.swift
+//  MiniApp82-SwiftUI-Binding&State
+//
+//  Created by 前田航汰 on 2022/08/14.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp82_SwiftUI_Binding_StateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
